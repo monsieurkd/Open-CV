7d4dcf1d90dbd6cbc7021cf9c4d09063d9c7e526
monsieurkd <87173358+monsieurkd@users.noreply.github.com>
2023-12-27 12:01:27 +0700

    Changed the code to use a different image and different transformation points.

--- threshold.py
+++ threshold.py
@@ -1,14 +1,33 @@
 import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+
 # Load two images
-img1 = cv.imread('picture/gray.jpg')
-img2 = cv.imread('picture/pepsi.png')
+# img1 = cv.imread('picture/gray.jpg')
+# img2 = cv.imread('picture/pepsi.png')
 
-#dynamic threshold
-gray = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
-adaptive_thresh = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY,9, 8)
-cv.imshow('thresh',adaptive_thresh )
+# #dynamic threshold
+# gray = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
+# adaptive_thresh = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY,9, 8)
+# cv.imshow('thresh',adaptive_thresh )
 
-
+#adaptive threshold
+img = cv.imread('picture/sudoku.PNG',0)
+img = cv.medianBlur(img,5)
+ret,th1 = cv.threshold(img,127,255,cv.THRESH_BINARY)
+th2 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_MEAN_C,\
+            cv.THRESH_BINARY,11,2)
+th3 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv.THRESH_BINARY,11,2)
+titles = ['Original Image', 'Global Thresholding (v = 127)',
+            'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
+images = [img, th1, th2, th3]
+for i in range(4):
+    plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+plt.show()
 
 cv.waitKey(0)
 cv.destroyAllWindows()
--- transformation.py
+++ transformation.py
@@ -22,13 +22,22 @@ rows,cols = img.shape
 
 #affine transformation
 #how to find this 3 point i have no idea
-pts1 = np.float32([[50,50],[200,50],[50,200]])
-pts2 = np.float32([[10,100],[200,50],[100,250]])
-M = cv.getAffineTransform(pts1,pts2)
-dst = cv.warpAffine(img,M,(cols,rows))
+# pts1 = np.float32([[50,50],[200,50],[50,200]])
+# pts2 = np.float32([[10,100],[200,50],[100,250]])
+# M = cv.getAffineTransform(pts1,pts2)
+# dst = cv.warpAffine(img,M,(cols,rows))
 
-plt.subplot(121),plt.imshow(img),plt.title('Input'), plt.plot(pts1, 'bo')
-plt.subplot(122),plt.imshow(dst),plt.title('Output'), plt.plot(pts2, 'bo')
+# plt.subplot(121),plt.imshow(img),plt.title('Input'), 
+# plt.subplot(122),plt.imshow(dst),plt.title('Output'), plt.imshow([10,100])
+# plt.show()
+
+#perspective transformation
+pts1 = np.float32([[56,65],[368,52],[28,387],[389,390]])
+pts2 = np.float32([[0,0],[300,0],[0,300],[300,300]])
+M = cv.getPerspectiveTransform(pts1,pts2)
+dst = cv.warpPerspective(img,M,(300,300))
+plt.subplot(121),plt.imshow(img),plt.title('Input')
+plt.subplot(122),plt.imshow(dst),plt.title('Output')
 plt.show()
 
 cv.waitKey(0)
