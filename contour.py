7e2a6035240526db8ae73bd850ef9280334cd914
Monsieurkd <kieuduc2505@gmail.com>
2024-01-03 21:41:00 +0700

    learning about contour 2/5, very frustrating

--- contour.py
+++ contour.py
@@ -1,23 +1,42 @@
-# import numpy as np
-# import cv2 as cv
-# im = cv.imread('test.jpg')
-# assert im is not None, "file could not be read, check with os.path.exists()"
-# imgray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
-# ret, thresh = cv.threshold(imgray, 127, 255, 0)
-# contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
 
 import cv2 as cv 
 import numpy as np
-image = cv.imread('picture/noisy2.PNG')
+image = cv.imread('picture/contour.png')
 assert image is not None, "file could not be read, bla bla"
 imgray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 ret, thresh = cv.threshold(imgray, 127, 255,  0)
 contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-cv.drawContours(imgray, contours, -1, (0,0,255), 3)
+cv.drawContours(imgray, contours, -1, (0,255,0), 3)
 # cnt = contours[4]
 # cv.drawContours(imgray, [cnt], 0, (0,255,0), 3)
 cv.imshow('imgray', imgray)
 
+#test.png return error, every value of moment return 0
 
+
+cnt = contours[0]
+M = cv.moments(cnt)
+print( M )
+
+
+#calculate centroid
+cx = int(M['m10']/M['m00'])
+cy = int(M['m01']/M['m00'])
+print(cx ,cy)
+#contour area
+print(cv.contourArea(cnt))
+
+#contour perimeter
+print(cv.arcLength(cnt, True))
+
+#contour approximation
+epsilon = 0.1*cv.arcLength(cnt,True)
+approx = cv.approxPolyDP(cnt,epsilon,True)
+
+
+
+
+# cv.imshow('approx', approx)
 cv.waitKey(0)
